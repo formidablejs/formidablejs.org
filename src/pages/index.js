da7e06ea294685870906cb75f654d2d7d823a031
Donald Pakkies <donaldpakkies@gmail.com>
2022-03-15 14:16:24 +0000

    feat: add live preview and features section

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,6 +43,55 @@ export default function Home() {
 					<img src="/img/main.png" className='img-code'/>
 				</div>
 
+				<div className='features'>
+					<div className="container">
+						<div className="grid-container">
+							<div style={{ flex: 1}}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+								</svg>
+								<h3>Craftsman</h3>
+								<p>
+									Easily build your Formidable application, tinker with your database data and control your application directly from the terminal using the
+									Craftsman Development CLI tool. Craftsman lets you easily scaffold common classes such as Controllers, Models, and Migrations.
+								</p>
+							</div>
+							<div style={{ flex: 1}}>
+								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
+								</svg>
+								<h3>Database</h3>
+								<p>
+									Formidable ships with the Knex.js Query Builder. It features both traditional node style callbacks as well as a promise interface for
+									cleaner async flow control, a stream interface, full-featured query and schema builders. It supports PostgreSQL, MSSQL, MySQL, SQLite3,
+									Oracle, and more.
+								</p>
+							</div>
+							<div style={{ flex: 1}}>
+								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
+								</svg>
+								<h3>Authentication</h3>
+								<p>
+									With Formidable's out-the-box authentication system, you can easily set up a secure, session based authentication for web applications.
+									You can also set up authentication for APIs and mobile applications with JWT token based authentication system.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="live">
+					<h1>
+						Live preview
+					</h1>
+					<p>
+						See how your application may potentially look like without leaving your personal browser.
+					</p>
+
+					<div dangerouslySetInnerHTML={{ __html: "<iframe src='https://codesandbox.io/embed/formidable-pn32bs?fontsize=14&autoresize=1&hidenavigation=1&module=%2Fresources%2Fimba%2FApp.imba' style='width:100%;height:500px;border:0;border-radius:4px;overflow:hidden;' title='formidable' allow='accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking' sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'></iframe>" }}/>
+				</div>
+
 				<div className='community'>
 					<div className="container">
 						<div className="grid-container">
