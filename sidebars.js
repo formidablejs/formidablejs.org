067281f237e00702284b189bdd554102037ce8ad
Donald Pakkies <donaldpakkies@gmail.com>
2022-10-16 13:35:45 +0200

    chore: remove ts support

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
 	docs: {
 		Prologue: [ 'releases', 'contributions' ],
-		'Getting Started': ['installation', 'configuration', 'structure', 'deployment', 'typescript-support'],
+		'Getting Started': ['installation', 'configuration', 'structure', 'deployment'],
 		Basics: ['routing', 'middleware', 'csrf-protection', 'controllers', 'requests', 'responses', 'views', 'validation'],
 		'Digging Deeper': ['craftsman', 'frontend', 'helpers', 'localization', 'mail', 'packages', 'hooks'],
 		Database: ['database-getting-started', 'database-query-builder', 'database-migrations', 'database-seeding'],
