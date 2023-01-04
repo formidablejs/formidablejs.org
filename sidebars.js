50627dfcd139eee1179079b25ff1d6f5cb10d478
Donald Pakkies <donaldpakkies@gmail.com>
2023-01-04 23:15:46 +0200

    chore: move frontend to getting started

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,9 @@
 module.exports = {
 	docs: {
 		Prologue: [ 'releases', 'contributions' ],
-		'Getting Started': ['installation', 'configuration', 'structure', 'deployment'],
+		'Getting Started': ['installation', 'configuration', 'structure', 'frontend', 'deployment'],
 		Basics: ['routing', 'middleware', 'csrf-protection', 'controllers', 'requests', 'responses', 'views', 'validation', 'logging'],
-		'Digging Deeper': ['craftsman', 'frontend', 'helpers', 'hooks', 'localization', 'mail', 'packages', 'scheduling'],
+		'Digging Deeper': ['craftsman', 'helpers', 'hooks', 'localization', 'mail', 'packages', 'scheduling'],
 		Database: ['database-getting-started', 'database-query-builder', 'database-migrations', 'database-seeding'],
 		Security: ['authentication', 'verification', 'encryption', 'hashing'],
 	}
