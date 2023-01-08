7b110923a12f58b9460d18c9bc818a1bcecf0231
Donald Pakkies <donaldpakkies@gmail.com>
2023-01-08 16:44:44 +0200

    chore: updated sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
 	docs: {
 		Prologue: [ 'releases', 'contributions' ],
-		'Getting Started': ['installation', 'configuration', 'structure', 'frontend', 'deployment'],
-		Basics: ['routing', 'middleware', 'csrf-protection', 'controllers', 'requests', 'responses', 'views', 'validation', 'logging'],
+		'Getting Started': ['installation', 'frontend', 'configuration', 'structure', 'deployment'],
+		Basics: ['routing', 'middleware', 'csrf-protection', 'controllers', 'requests', 'responses', 'validation', 'views', 'spa-hooks', 'logging'],
 		'Digging Deeper': ['craftsman', 'helpers', 'hooks', 'localization', 'mail', 'packages', 'scheduling'],
 		Database: ['database-getting-started', 'database-query-builder', 'database-migrations', 'database-seeding'],
 		Security: ['authentication', 'verification', 'encryption', 'hashing'],
